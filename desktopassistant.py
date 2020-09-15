79be623249b832ea80747286f26bd961f264b5b8
Soumyajit Basu (C0r3f!nd3r) <soumyajit.basu62@gmail.com>
2020-09-15 22:18:26 +0530

    Add voice utility and engine class 	modified:   .gitignore 	new file:   data.py 	modified:   desktopassistant.py 	new file:   engine/__init__.py 	new file:   engine/__init__.pyc 	new file:   engine/engine.py 	new file:   engine/engine.pyc 	new file:   utility/__init__.py 	new file:   utility/__init__.pyc 	new file:   utility/desktoputility.py 	new file:   utility/desktoputility.pyc

--- desktopassistant.py
+++ desktopassistant.py
@@ -1,18 +1,27 @@
-import pyttsx3
+from utility.desktoputility import Desktoputility
+from engine.engine import Engine
+import data
+import sys
 
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-for voice in voices:
-    if voice.languages[0] == u'en_US':
-        engine.setProperty('voice', voice.id)
-        break
+if __name__ == "__main__":
+    en = Engine()
+    du = Desktoputility()
 
+    # Greet user before starting
+    du.greeting()
+    en.speak('Nova here')
+    en.speak('What would you like me to do for you ?')
 
-def speak(audio):
-    print("Nova: " + audio)
-    engine.say(audio)
-    engine.runAndWait()
-    engine.stop()
+    while True:
+        query = en.command()
+        query = query.lower()
 
+        if query in data.voice_greeting:
+            du.greetback(query)
+            if "good night" in query or "night" in query:
+                en.speak("Have a tight sleep")
+                sys.exit()
 
-speak("Dothraki tribe of Anges way up in the mountains")
+        if query in data.voice_data_exit:
+            en.speak('Alright. Have a nice day')
+            sys.exit()
