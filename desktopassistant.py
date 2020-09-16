3ab4786488b3541ecd2cda2413653f6a8f3974c7
Soumyajit Basu (C0r3f!nd3r) <soumyajit.basu62@gmail.com>
2020-09-16 09:22:35 +0530

    Change data format and modify cbase 4 desktopasst 	new file:   data/__init__.py 	renamed:    data.py -> data/data.py 	modified:   desktopassistant.py

--- desktopassistant.py
+++ desktopassistant.py
@@ -1,6 +1,6 @@
 from utility.desktoputility import Desktoputility
 from engine.engine import Engine
-import data
+from data import data
 import sys
 import getpass
 
@@ -12,20 +12,23 @@ if __name__ == "__main__":
 
     # Greet user before starting
     du.greeting()
-    en.speak('Nova here')
-    en.speak('What would you like me to do for you ?')
+    en.speak("Nova here")
+    en.speak("What would you like me to do for you ?")
 
     while True:
         query = en.command()
         query = query.lower()
 
+        # Voice greeting
         if query in data.voice_greeting:
-            du.greetback(query)
+            du.greetback(query+" "+sysuser)
 
+        # Exit criteria
         if query in data.voice_data_exit:
             en.speak("Alright " + sysuser + "!! Have a nice day")
             sys.exit()
 
+        # Exit if user is wishing good night
         if "good night" in query or "night" in query:
             en.speak("Good night " + sysuser + ". Have a tight sleep")
             sys.exit()
