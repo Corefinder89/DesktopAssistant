cf505c039f504aa5925d319cb8189c31dd0ea78c
Soumyajit Basu (C0r3f!nd3r) <soumyajit.basu62@gmail.com>
2020-09-16 13:17:37 +0530

    Make a OOPS based approach for the ep class 	modified:   desktopassistant.py

--- desktopassistant.py
+++ desktopassistant.py
@@ -1,48 +1,56 @@
-from utility.desktoputility import Desktoputility
-from engine.engine import Engine
-from data import data
-import sys
+# -*- coding: utf-8 -*-
 import getpass
+import sys
+
+from data import data
+from engine.engine import Engine
+from utility.desktoputility import Desktoputility
 
 sysuser = getpass.getuser()
 
+
+class Desktopassistant:
+    def __init__(self):
+        self.en = Engine()
+        self.du = Desktoputility()
+
+    def desktop_main(self):
+        # Greet user before starting
+        self.du.greeting()
+        self.en.speak("Nova here")
+        self.en.speak("What would you like me to do for you ?")
+
+        while True:
+            query = self.en.command()
+            query = query.lower()
+
+            # Voice greeting
+            if query in data.voice_greeting:
+                self.du.greetback(query + " " + sysuser)
+
+            # Exit criteria
+            if query in data.voice_data_exit:
+                self.en.speak("Alright " + sysuser + "!! Have a nice day")
+                sys.exit()
+
+            # Exit if user is wishing good night
+            if "good night" in query or "night" in query:
+                self.en.speak("Good night " + sysuser + ". Have a tight sleep")
+                sys.exit()
+
+            # Search on Google
+            if query in data.voice_search:
+                outputList = []
+                self.en.speak('What should I search for ?')
+                query = self.en.command()
+                self.du.searchOnGoogle(query, outputList)
+                self.en.speak('Should I open up the first link for you ?')
+                query = self.en.command()
+                if query in data.boolean_affirmitive:
+                    self.du.openLink(outputList)
+                if query in data.boolean_negate:
+                    self.en.speak('Alright.')
+
+
 if __name__ == "__main__":
-    en = Engine()
-    du = Desktoputility()
-
-    # Greet user before starting
-    du.greeting()
-    en.speak("Nova here")
-    en.speak("What would you like me to do for you ?")
-
-    while True:
-        query = en.command()
-        query = query.lower()
-
-        # Voice greeting
-        if query in data.voice_greeting:
-            du.greetback(query+" "+sysuser)
-
-        # Exit criteria
-        if query in data.voice_data_exit:
-            en.speak("Alright " + sysuser + "!! Have a nice day")
-            sys.exit()
-
-        # Exit if user is wishing good night
-        if "good night" in query or "night" in query:
-            en.speak("Good night " + sysuser + ". Have a tight sleep")
-            sys.exit()
-
-        # Search on Google
-        if query in data.voice_search:
-            outputList = []
-            en.speak('What should I search for ?')
-            query = en.command()
-            du.searchOnGoogle(query, outputList)
-            en.speak('Should I open up the first link for you ?')
-            query = en.command()
-            if query in data.boolean_affirmitive:
-                du.openLink(outputList)
-            if query in data.boolean_negate:
-                en.speak('Alright.')
-
+    Desktopassistant().desktop_main()
